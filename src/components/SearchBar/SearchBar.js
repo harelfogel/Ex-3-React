3112e33a99757fffad98a6bfb4e5047467a3a935
harelfogel <fogell06@gmail.com>
2022-01-03 19:12:57 +0200

    end design and start functionlity

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SearchBar.css';
 import { FiSearch } from 'react-icons/fi';
+import vacationsData from './../Data/vacations';
 
-const SearchBar = () => {
-    return <div className="search-bar">
-        <div className="search-container">
-            <i className="search-icon"> <FiSearch style={{color:'#AFAFAF'}}/></i>
-            <input type="text" className="search-term" placeholder="Search name or location"/>
-        </div>
-            <button type="submit" className="search-button">
-                <FiSearch style={{color:"#FFFFFF",width:"18px", height:"18px"}}/>
+
+const SearchBar = (props) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleFilter = () => {
+
+        const newFilter = vacationsData.filter((value) => {
+            return value.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        props.setFilteredData(newFilter);
+    };
+
+    useEffect (() =>{
+        console.log(searchTerm);
+        if(searchTerm===""){
+            props.setFilteredData([]);
+        }
+    }, [searchTerm])
+
+    return (
+        <div className="search-bar">
+            <div className="search-container">
+                <i className="search-icon"> <FiSearch style={{ color: '#AFAFAF' }} /></i>
+                <input type="text" className="search-term" placeholder="Search name or location" onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+            <button type="submit" className="search-button" onClick={handleFilter}>
+                <FiSearch style={{ color: "#FFFFFF", width: "18px", height: "18px" }} />
             </button>
-    </div>;
+        </div>);
+
 }
 
 export default SearchBar;
--- src/components/VacationList/VacationList.js
+++ src/components/VacationList/VacationList.js
@@ -2,60 +2,73 @@ import React from 'react'
 import Card from '../Card/Card';
 import vacationsData from './../Data/vacations';
 import './VacationList.css';
-const VacationList = () => {
-    const data = [
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    },
-    {
-        name:"Paris",
-        image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
-        location:"France",
-        price:"$1,000"
-    }
-    ];
-    return <div className="list">{vacationsData.length !== 0 && vacationsData.map((dest) => {
-        return <Card data={dest} />;
-    })}</div>;
+const VacationList = (props) => {
+    //const data = [
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // },
+    // {
+    //     name:"Paris",
+    //     image:"https://www.voyagesdereve.ch/upload/images/xParis-Tour-Eiffel.jpg.pagespeed.ic.2trsrFlXuR.jpg",
+    //     location:"France",
+    //     price:"$1,000"
+    // }
+    // ];
+    return (
+        <div className="list">
+            {
+                props.vacationList.length !== 0 ? props.vacationList.map((dest) => {
+                    return <Card data={dest} setEditMode={props.setEditMode} />
+                }) :
+                    vacationsData.length !== 0 && vacationsData.map((dest) => {
+                        return <Card data={dest} setEditMode={props.setEditMode} />;
+                    })
+            }
+        </div>
+    )
+
+    // return <div className="list">{vacationsData.length !== 0 && vacationsData.map((dest) => {
+    //     return <Card data={dest} />;
+    // })}</div>;
 }
 
 export default VacationList;
